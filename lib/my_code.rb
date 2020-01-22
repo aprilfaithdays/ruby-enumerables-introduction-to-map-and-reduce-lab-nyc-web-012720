ca9536da9b9d517e088af10974e76d525d271be1
April Escobar <aprilfaith.e@gmail.com>
2020-01-22 03:53:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,7 +54,12 @@ end
 
 def reduce_to_all_true(source_array)
   # return source_array.all?()
-  
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
 end
 
 
